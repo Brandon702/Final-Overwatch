3469a2533d70df6cb0921a96412d2ebe1d0e129f
Michael Jones <mrj705@outlook.com>
2020-02-27 15:49:41 -0700

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,5 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
         //Test, this is Brandon, please make a note as well so I know you guys can access this file
+        //Michael here
     }
 }
