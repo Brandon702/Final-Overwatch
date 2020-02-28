1d20c1a85b9870b157b88f1e74341b7d2f76573e
Brandon Goldsworthy <BGoldsworthy@neumont.edu>
2020-02-28 11:24:59 -0700

    First upload, just a test to make sure commit works

--- src/com/company/View/View.java
+++ src/com/company/View/View.java
@@ -27,13 +27,31 @@ public class View {
                            "...........................|..|.................................................\n" +
                            "...........................|..|.................................................\n");
         System.out.println("\033[31;1mHello\033[0m, \033[32;1;2mworld!\033[0m");
-        System.out.println("\033[30mThis is the color White");
-        System.out.println("\033[31mThis is the color red");
-        System.out.println("\033[32mThis is the color green");
-        System.out.println("\033[33mThis is the color Yellow");
-        System.out.println("\033[34mThis is the color Blue");
-        System.out.println("\033[35mThis is the color Light purple?");
-        System.out.println("\033[36mThis is the color cyan");
-        System.out.println("\033[37mThis is the color Grey");
+        System.out.println("This is the default color");
+        System.out.println("\033[1mBold\033[0m");
+        System.out.println("\033[3mSlanted\033[0m");
+        System.out.println("\033[4mUnderline\033[0m");
+        System.out.println("\033[7mHighlight\033[0m");
+        System.out.println("\033[9mCrossout\033[0m");
+        System.out.println("\033[20mLatin? es tu brutus\033[0m");
+        System.out.println("\033[21mBold Underline\033[0m");
+        System.out.println("\033[30mThis is the color White\033[0m");
+        System.out.println("\033[31mThis is the color red\033[0m");
+        System.out.println("\033[32mThis is the color green\033[0m");
+        System.out.println("\033[33mThis is the color Yellow\033[0m");
+        System.out.println("\033[34mThis is the color Blue\033[0m");
+        System.out.println("\033[35mThis is the color Light purple?\033[0m");
+        System.out.println("\033[36mThis is the color cyan\033[0m");
+        System.out.println("\033[37mThis is the color Grey\033[0m");
+        System.out.println("\033[40mHighlight White\033[0m");
+        System.out.println("\033[41mHighlight Red\033[0m");
+        System.out.println("\033[42mHighlight Green\033[0m");
+        System.out.println("\033[43mHighlight Yellow\033[0m");
+        System.out.println("\033[44mHighlight Blue\033[0m");
+        System.out.println("\033[45mHighlight Purple\033[0m");
+        System.out.println("\033[46mHighlight Cyan\033[0m");
+        System.out.println("\033[47mHighlight Grey\033[0m");
+        System.out.println("\033[51mFramed words\033[0m");
+        System.out.println("\033[52mEncircled words\033[0m");
     }
 }
