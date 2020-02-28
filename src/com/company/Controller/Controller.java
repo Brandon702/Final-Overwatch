02238cb03c470f915156e67861669245ac67adfd
Brandon Goldsworthy <BGoldsworthy@neumont.edu>
2020-02-27 18:02:13 -0700

    First upload, just a test to make sure commit works

--- src/com/company/Controller/Controller.java
+++ src/com/company/Controller/Controller.java
@@ -1,4 +1,7 @@
 package com.company.Controller;
 
+import com.company.View.View;
+
 public class Controller {
+    View view = new View();
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,9 +1,12 @@
 package com.company;
 
-public class Main {
+import com.company.View.View;
 
+public class Main {
     public static void main(String[] args) {
         //Test, this is Brandon, please make a note as well so I know you guys can access this file
         //Michael here
+        View view = new View();
+        view.test();
     }
 }
