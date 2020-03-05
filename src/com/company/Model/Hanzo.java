857c084b13cdcc4bdf2172aed9e4024bd74c2977
Michael Jones <mrj705@outlook.com>
2020-03-05 11:00:58 -0700

    Hanzo Done

--- src/com/company/Model/Hanzo.java
+++ src/com/company/Model/Hanzo.java
@@ -3,6 +3,52 @@ package com.company.Model;
 public class Hanzo extends Hero {
 
     public Hanzo() {
-        super("Hanzo", "characterInfo", "characterWeapon", "eAbilityInfo", "shiftAbilityInfo", "rightClickAbilityInfo", "qAbilityInfo", "characterTips");
+        super("Hanzo",
+                "Real Name: Hanzo Shimada\n" +
+                "Age: 38\n" +
+                "Nationality: Japanese\n" +
+                "Health: 200\n" +
+                "Role: Damage\n" +
+                "Hanzo's passive ability is Wall Climb: 7.15 meters per second (vertical) and up to 4 meters vertical climb\n",
+                "Hanzo's Primary Weapon is his Storm Bow\n" +
+                "Type: Arcing Projectile\n" +
+                "Damage: 27.2 - 125 (depending on charge)\n" +
+                "Spread Angle: Pinpoint\n" +
+                "Movement Speed while aiming: -30% penalty\n" +
+                "Projectile Speed: 25 - 100 m/s\n" +
+                "Rate of Fire: Up to 0.75 seconds to charge, 0.5 second recovery\n" +
+                "Ammo: Unlimited\n" +
+                "Headshot: Yes\n",
+                "Hanzo's E Ability is Storm Arrows\n" +
+                "Type: Arching Projectile\n" +
+                "Damage: 60\n" +
+                "Spread Angle: Pinpoint\n" +
+                "Projectile Speed: 110 meters per second\n" +
+                "Rate of fire: 1 shot per 0.3 seconds (3.33 shots per second)\n" +
+                "Ammo: 5\n" +
+                "Duration: Up to 5 seconds\n" +
+                "Cooldown: 10 seconds\n" +
+                "Headshot: Yes\n",
+                "Hanzo's Shift Ability is Sonic Arrow\n" +
+                "Type: Arching Projectile\n" +
+                "Area of Effect: 9 meter radius\n" +
+                "Duration: 6 seconds\n" +
+                "Cooldown: 12 seconds\n",
+                "Hanzo's Spacebar Ability is Lunge\n" +
+                "Movement Speed: 15 meter per second(horizontal)\n" +
+                "Max Range: 7.85 meters\n" +
+                "Cooldown: 5 seconds\n",
+                "Hanzo's Q Ability (Ult) is Dragonstrike\n" +
+                "Type: Projectile: Initial shot, Area of Effect: Dragons\n" +
+                "Damage: Arrow: 125, Dragons: 150 per second, per dragon and up to 993.6 damage\n" +
+                "against a stationary target\n" +
+                "Projectile Speed: 20 meters per second\n" +
+                "Max Range: Infinite travel distance\n" +
+                "Area of Effect: 4 meter radius\n" +
+                "Rate of Fire: 1.5 charging\n" +
+                "Casting time: 0.9 seconds (charging), 0.6 seconds(arrow before becoming Dragons)\n" +
+                "Headshot: Arrow: Yes, Dragons: No\n" +
+                "Ultimate cost: 1680 points or 1% ult charge every 3.36 seconds passively\n",
+                "characterTips");
     }
 }
