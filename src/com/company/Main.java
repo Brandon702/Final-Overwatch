677b1afe6cfc93da1bd15bb7786aa68f0736a524
Brandon Goldsworthy <BGoldsworthy@neumont.edu>
2020-03-09 15:28:55 -0600

    Throws Exception now

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,8 +6,10 @@ import com.company.Model.McCree;
 import com.company.Model.Reinhardt;
 import com.company.View.View;
 
+import java.io.IOException;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         Controller controller = new Controller();
         controller.run();
     }
