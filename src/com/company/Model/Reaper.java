e353d0a8a87c437ec760883d144b49a1e00dcb8f
Michael Jones <mrj705@outlook.com>
2020-03-09 18:47:24 -0600

    Reaper is Finished

--- src/com/company/Model/Reaper.java
+++ src/com/company/Model/Reaper.java
@@ -3,6 +3,52 @@ package com.company.Model;
 public class Reaper extends Hero {
 
     public Reaper() {
-        super("Reaper", "characterInfo", "characterWeapon", "eAbilityInfo", "shiftAbilityInfo", "rightClickAbilityInfo", "qAbilityInfo", "characterTips");
+        super("Reaper\n",
+                "Real Name: Gabriel Reyes\n" +
+                "Age: 58\n" +
+                "Nationality: American\n" +
+                "Health: 250\n" +
+                "Role: Damage\n" +
+                "Reaper has a Passive Ability called The Reaping: Heals 30% of damage dealt\n",
+                "Reaper's Primary Weapon is his Hellfire Shotguns\n" +
+                "Type: Shotgun (hitscan)\n" +
+                "Damage: 2.1 - 7 per pellet\n" +
+                "42 - 140 per shot\n" +
+                "Falloff Range: 10 to 20 meters\n" +
+                "Spread Angle: Constant 20.1 degrees\n" +
+                "Number of Pellets: 20 per shot\n" +
+                "Rate of Fire: 2 shots per second\n" +
+                "Ammo: 8\n" +
+                "Reload Time: 1.5 seconds\n" +
+                "Headshot: Yes\n",
+                "Reaper's E Ability is Shadow Step\n" +
+                "Max Range: 35 meters\n" +
+                "Casting time: 1.5 seconds\n" +
+                "Duration: 2 seconds\n" +
+                "Cooldown: 12 seconds\n",
+                "Reapers Shift Ability is Wraith Form\n" +
+                "Movement Speed: +50% buff\n" +
+                "Duration: Up to 3 seconds\n" +
+                "Cooldown: 8 seconds\n",
+                "Reaper has no Right Click Ability\n",
+                "Reaper's Q Ability (Ult) is Death Blossom\n" +
+                "Type: Area of Effect (AOE)\n" +
+                "Damage: 170 damage per second\n" +
+                "(34 damage per hit, 5 hits per second, per target)\n" +
+                "510 damage overall\n" +
+                "Movement Speed: -50% nerf\n" +
+                "Area of Effect: 8 meter Radius\n" +
+                "Duration: 3 seconds\n" +
+                "Headshot: No\n" +
+                "Ultimate Cost: 2100 points or 1% charge every 4.20 seconds passively\n",
+                "Here are a few tips for a beginner Reinhardt player\n" +
+                "Tip 1: Reaper's Death Blossom is most effective in the middle of a team fight when it is least expected. If you can get the team mid fight, best use.\n" +
+                "However, any sort of barrier (shield, environment, or payload) or any Crowd Control ability (Mei freezing or wall, Brigs whip, or a Rein charge) can either make your\n" +
+                "ult wasted, kill you, or make you immobile so watch out for that.\n" +
+                "Tip 2: Reaper's Shadow Step is a personal teleporter. You can use it to take or contest high ground or use it to get a flank on your enemy tanks because Reaper\n" +
+                "is most effective against tanks.\n" +
+                "Tip 3: Characters with stun abilities or other Crowd Control abilities (McCree, Mei, Brig) are very effective against you so avoid them if you don't know\n" +
+                "if their abilities are on cooldown and any squishy character (someone under 250 base HP) aren't great for Reaper. You get the most out of attacking the enemy tanks.\n" +
+                "That doesn't mean avoid DPS and Support characters, just focus on tanks first.");
     }
 }
